15af3487f0e233a4ea808508cf761cf5664fe7e9
AndersonR-S <andersonlagoagrandesantos@gmail.com>
2024-12-09 23:54:26 -0300

    MIC2

--- ULA/simulation/qsim/work/@d@e@c@o@d@e@r2_4_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity DECODER2_4_vlg_vec_tst is
-end DECODER2_4_vlg_vec_tst;
--- ULA/simulation/qsim/work/@u@l@a_32_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity ULA_32_vlg_vec_tst is
-end ULA_32_vlg_vec_tst;
